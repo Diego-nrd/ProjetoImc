88e2f6da8992d65df2c9a29c9c90fc9c5a1f6772
Diego <diego-11-0@hotmail.com>
2024-10-09 15:43:47 -0300

    Implementação de modo noturno e mais correções

--- Js/script.js
+++ Js/script.js
@@ -37,8 +37,7 @@ function imc(tipo, op) {
         const resutImc = document.getElementById("calculo_imc");
         const resutTextImc = document.getElementById("texto_imc");
         const text_dica = document.getElementById("texto_dica");
-        //const ponteiro = document.getElementById("ponteiro");
-
+        
         const imc = parseFloat(v_peso / (v_altura * v_altura)).toFixed(2)
 
         const resultSections = ['baixo', 'normal', 'sobre_pes', 'obesidade', 'obesidade2'];
@@ -51,13 +50,12 @@ function imc(tipo, op) {
         let text = "";
         let displayId = "";
         let dica = "";
-        let ponteiro = "z 0deg";
         let grau;
 
         if (imc <= 18.49) {
             displayId = 'baixo';
             backgroundColor = "#74b6d8";
-            backgroundColor2 = '#74b6d845'
+            //backgroundColor2 = '#74b6d845'
             text = "Abaixo do peso!";
             grau = ( imc  / 20) * 31
             dica = "O seu peso não está combinando com sua altura.<br>" +
@@ -72,7 +70,7 @@ function imc(tipo, op) {
         } else if (imc >= 18.50 && imc <= 24.99) {
             displayId = 'normal';
             backgroundColor = "#26c7a4";
-            backgroundColor2 = "#26c7a461"
+            //backgroundColor2 = "#26c7a461"
             text = "Peso Ideal!";
             grau = ((imc - 10) / 11.2) * 52.2;
             dica = "O seu peso está ideal com sua altura!<br>" +
@@ -83,7 +81,7 @@ function imc(tipo, op) {
         } else if (imc >= 25 && imc <= 29.99) {
             displayId = 'sobre_pes';
             backgroundColor = "#ffd900";
-            backgroundColor2 = "#ffd90057";
+            //backgroundColor2 = "#ffd90057";
             text = "Sobre Peso!";
             grau = ((imc - 10) / 11.7) * 61.4;
             dica = "O seu peso está um pouco acima do ideal!<br> " +
@@ -98,7 +96,7 @@ function imc(tipo, op) {
         } else if (imc >= 30 && imc <= 39.99) {
             displayId = 'obesidade';
             backgroundColor = "#fea04a";
-            backgroundColor2 = "#fea04a80";
+            //backgroundColor2 = "#fea04a80";
             text = "Obesidade!";
             grau = ((imc - -5) / 4.9) * 15.5;
             dica = "O seu peso está acima do Ideal!<br>" +
@@ -110,7 +108,7 @@ function imc(tipo, op) {
         } else if (imc >= 40){
             displayId = 'obesidade2';
             backgroundColor = "#ff454d";
-            backgroundColor2 = "#ff454f85"
+            //backgroundColor2 = "#ff454d82"
             text = "Obesidade Grave!";
             grau = ((imc - -374) / 15 ) * 5.4;
             dica = "Sinal vermelho!!!<br>" +
@@ -121,14 +119,19 @@ function imc(tipo, op) {
             alert('Nop')
         }
 
-    
         document.getElementById(displayId).style.display = "block";
         document.getElementById('col_img').style.display = "block";
         document.getElementById('dados_p').style.backgroundColor = backgroundColor;
-        document.getElementById('dica_imc').style.backgroundColor = backgroundColor2;
+        document.getElementById('dica_imc').style.color = backgroundColor;
         document.getElementById('ponteiro').style.rotate = grau = `z ${grau}deg`;
         resutImc.innerHTML = imc
         resutTextImc.innerHTML = text;
         text_dica.innerHTML = dica;
     }
 }
+
+function dark_mode(){
+    const body = document.body;
+    body.classList.toggle("dark_mode");
+ }
+ 
