133a8fa14f0b63fd3fa8bcb40cdaa1dfc9787579
Diego <diego-11-0@hotmail.com>
2024-10-11 22:27:51 -0300

    intentacao ao codigo fonte

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,6 @@
 function sepAlt(input) {
     let nums = input.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1')
         .replace(/\B(?=(\d{2})+(?!\d))/g, '.')
-
     input.value = nums
 }
 
@@ -44,9 +43,7 @@ function imc(tipo, op) {
         resultSections.forEach(id => document.getElementById(id).style.display = "none");
         document.getElementById('col_img').style.display = "none";
 
-
         let backgroundColor = "";
-        let backgroundColor2 = "";
         let text = "";
         let displayId = "";
         let dica = "";
@@ -64,24 +61,18 @@ function imc(tipo, op) {
                 "1º Se alimente corretamente com alimentos saudáveis.<br>" +
                 "2º Pratique Exercicios físico para o aumento da massa muscular.<br>" +
                 "O Mais recomendado seria a procura de um médico, para um auxilio correto."
-
-
-
         } else if (imc >= 18.50 && imc <= 24.99) {
             displayId = 'normal';
             backgroundColor = "#26c7a4";
-            //backgroundColor2 = "#26c7a461"
             text = "Peso Ideal!";
             grau = ((imc - 10) / 11.2) * 52.2;
             dica = "O seu peso está ideal com sua altura!<br>" +
                 "Continue assim o seu peso está normal, " +
                 "a dica é mantenha seu peso, e seu estilo de vida " +
                 "com alimentação equilibrada e exercicios físicos!"
-
         } else if (imc >= 25 && imc <= 29.99) {
             displayId = 'sobre_pes';
             backgroundColor = "#ffd900";
-            //backgroundColor2 = "#ffd90057";
             text = "Sobre Peso!";
             grau = ((imc - 10) / 11.7) * 61.4;
             dica = "O seu peso está um pouco acima do ideal!<br> " +
@@ -91,12 +82,9 @@ function imc(tipo, op) {
                 "1º Se alimentar corretamente, evitar alimentos de alto sódio e gorduras.<br>" +
                 "2º Praticar exercicios físicos para a perca de gordura.<br>" +
                 "O Mais recomendado seria a procura de um nutricionista, para um auxilio correto."
-
-
         } else if (imc >= 30 && imc <= 39.99) {
             displayId = 'obesidade';
             backgroundColor = "#fea04a";
-            //backgroundColor2 = "#fea04a80";
             text = "Obesidade!";
             grau = ((imc - -5) / 4.9) * 15.5;
             dica = "O seu peso está acima do Ideal!<br>" +
@@ -104,21 +92,17 @@ function imc(tipo, op) {
                 "tem que se cuida melhor, mesmo que os exames estejam normais.<br> " +
                 "Cuide de sua alimentação, e pratique exercicios físicos.<br> " +
                 "Recomendamos que procure um nutricionista, para um auxilio correto.";
-
         } else if (imc >= 40) {
             displayId = 'obesidade2';
             backgroundColor = "#ff454d";
-            //backgroundColor2 = "#ff454d82"
             text = "Obesidade Grave!";
             grau = ((imc - -374) / 15) * 5.4;
             dica = "Sinal vermelho!!!<br>" +
                 "Possibilidade de existirem doenças graves<br>" +
                 "O tratamento deve ser o mais urgente.";
-
         } else {
             alert('Nop')
         }
-
         document.getElementById(displayId).style.display = "block";
         document.getElementById('col_img').style.display = "block";
         document.getElementById('dados_p').style.backgroundColor = backgroundColor;
@@ -133,7 +117,6 @@ function imc(tipo, op) {
 function dark_mode() {
     const body = document.body;
     body.classList.toggle("dark_mode");
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
